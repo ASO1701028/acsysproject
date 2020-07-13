9749e6ba610f8e7a20b929b77c3a4bd095dd8696
raige <raigekka0128@yahoo.co.jp>
2020-07-14 02:30:54 +0900

    Update StartTopの完成 add StartTopImage.jpgの追加

--- src/components/SrartTop.vue
+++ src/components/SrartTop.vue
@@ -1,15 +1,34 @@
 <template>
     <div id="#app">
-        Hello world
+        <div class="jumbotron-extend">
+            <div class="container-fluid jumbotron-container">
+                <h1>カロリーを管理してみませんか？</h1>
+                <p><a class="btn btn-outline-warning btn-lg" href="/#/userguide" role="button">使い方について</a></p>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-    export default {
-        name: "SrartTop"
-    }
-</script>
-
 <style scoped>
+
+    .jumbotron-extend {
+        position: relative;
+        height: 91.3vh;
+        min-height: 300px;
+        background: url(../assets/StartTopImage.jpg) no-repeat center center;
+        background-size: cover;
+        color: white;
+    }
+
+    .jumbotron-container {
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    #StartUp{
+        position: relative;
+        top: 10px;
+    }
 
 </style>
