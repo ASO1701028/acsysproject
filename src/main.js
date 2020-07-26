186138d49f3504781eb7beb1ad78fb54acd6ddff
raige <raigekka0128@yahoo.co.jp>
2020-07-27 04:20:01 +0900

    +Update main.jsにStoreの追加

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 // bootstrapの仕様
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
