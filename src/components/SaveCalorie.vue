8732c6f38e9fad853b97930bb0c4a71da6571a4c
raige <raigekka0128@yahoo.co.jp>
2020-07-31 10:24:37 +0900

    +Add SaveCalorieChart.vueを追加

--- src/components/SaveCalorie.vue
+++ src/components/SaveCalorie.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-
+        <p>現在の貯金は{{totalCalorie}}kcal</p>
+        <p>今日の貯金は{{todayCalorie}}kcal</p>
+        <p>摂取カロリー：{{todayPlusCalorie}}kcal</p>
+        <p>消費カロリー：{{todayMinusCalorie}}kcal</p>
     </div>
 </template>
 
@@ -8,6 +11,17 @@
 
     export default {
         name: "save_calorie",
+        data(){
+            return{
+                totalCalorie:"120",
+                todayPlusCalorie:200,
+                todayMinusCalorie:150,
+                todayCalorie:0,
+            }
+        },
+        created() {
+            this.todayCalorie = this.todayMinusCalorie - this.todayPlusCalorie
+        },
     }
 </script>
 
