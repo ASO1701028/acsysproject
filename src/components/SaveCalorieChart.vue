afb642ca52091d6fef2af340edb3a66efeb1c103
raige <raigekka0128@yahoo.co.jp>
2020-08-03 12:24:19 +0900

    +Update SaveCalorieChart.vueにチャートのテンプレを追加 +Update SaveCalorie.vueに通信とデザイン以外を実装

--- src/components/SaveCalorieChart.vue
+++ src/components/SaveCalorieChart.vue
@@ -1,13 +1,15 @@
-<template>
-    
-</template>
-
 <script>
+    import { Bar, mixins } from 'vue-chartjs'
+    const { reactiveProp } = mixins
+
     export default {
-        name: "SaveCalorieChart"
+        extends: Bar,
+        mixins: [reactiveProp],
+        props: ['options'],
+        mounted () {
+            // this.chartData is created in the mixin.
+            // If you want to pass options please create a local options object
+            this.renderChart(this.chartData, this.options)
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
