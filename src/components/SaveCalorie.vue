d2a2dc3cfcd98029f5be80648533b3d2f8f302e3
raige <raigekka0128@yahoo.co.jp>
2020-08-15 05:31:53 +0900

    +Update SaveCalorie.vueのデザイン完成

--- src/components/SaveCalorie.vue
+++ src/components/SaveCalorie.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
-        <p>現在の貯金は{{totalCalorie}}kcal</p>
-        <p>今日の貯金は{{todayCalorie}}kcal</p>
-        <p>摂取カロリー：{{todayPlusCalorie}}kcal</p>
-        <p>消費カロリー：{{todayMinusCalorie}}kcal</p>
-        <div class="chart-small">
-            <SaveCalorieChart :chart-data="dataCollection" :options="dataOptions"></SaveCalorieChart>
+    <div class="container">
+        <h2 class="pt-5 pb-4">現在の貯金は<span class="h1">{{totalCalorie}}</span>kcal</h2>
+        <div class="row">
+            <ul class="col-lg-6 col-auto list-group">
+                <li class="list-group-item list-group-item-primary lead">今日の貯金:{{todayCalorie}}kcal</li>
+                <li class="list-group-item list-group-item-info lead">ー摂取カロリー：{{todayPlusCalorie}}kcal</li>
+                <li class="list-group-item list-group-item-danger lead">＋消費カロリー：{{todayMinusCalorie}}kcal</li>
+                <li class="list-group-item  mb-4 lead">{{comment}}</li>
+                <a class="btn btn-outline-info btn-lg mb-4 " href="/#/intakecalorie" role="button">摂取カロリー登録 ー</a>
+                <a class="btn btn-outline-danger btn-lg mb-4" href="/#/consumptioncalorie" role="button">消費カロリー登録 ＋</a>
+            </ul>
+            <div class="chart-small col-lg-6 col-auto">
+                <SaveCalorieChart :chart-data="dataCollection" :options="dataOptions"></SaveCalorieChart>
+            </div>
         </div>
-        <a class="btn btn-info btn-lg" href="/#/intakecalorie" role="button">摂取カロリー登録</a>
-        <a class="btn btn-danger btn-lg" href="/#/consumptioncalorie" role="button">消費カロリー登録</a>
+
     </div>
 </template>
 
@@ -28,6 +34,7 @@
                 todayPlusCalorie:200,
                 todayMinusCalorie:150,
                 todayCalorie:0,
+                comment:"",
                 //グラフの関数
                 dataCollection: null,
                 dataOptions:null,
@@ -37,16 +44,21 @@
         },
         created() {
             this.todayCalorie = this.todayMinusCalorie - this.todayPlusCalorie
+            if (this.totalCalorie>0){
+                this.comment = "いい感じに貯金が貯まってきましたね！無理をせずこの調子で頑張っていきましょう。"
+            }else{
+                this.comment = "貯金がマイナスになってしまいましたね。こんな時は運動する量を増やしたり、食事を見直してみたりしましょう！"
+            }
         },mounted () {
             this.fillData()
         },
         methods: {
             fillData () {
                 this.dataCollection = {
-                    labels: ['消費カロリー', '摂取カロリー'],
+                    labels: ['＋ 消費カロリー', 'ー 摂取カロリー'],
                     datasets: [
                         {
-                            backgroundColor: '#59cd3b',
+                            backgroundColor: '#ff7e00',
                             data: [this.todayMinusCalorie,this.todayPlusCalorie]
                         },
                     ],
@@ -57,7 +69,7 @@
                         display: true,
                         text: "今日のデータ",
                         padding: 1,
-                        fontSize: 18
+                        fontSize: 20
                     },
 
                     scales: {
@@ -75,8 +87,8 @@
                                 scaleLabel: {
                                     //表示されるy軸の名称について
                                     display: true, //表示するか否か
-                                    labelString: "k/cal",
-                                    fontSize: 15
+                                    labelString: "kcal",
+                                    fontSize: 20
                                 }
                             }
                         ],
@@ -86,7 +98,8 @@
                                 ticks: {
                                     autoSkip: false, //横幅が狭くなったときに表示を間引くか否か
                                     maxRotation: 90, //下のと合わせて表示される角度を決める
-                                    minRoation: 90 //横幅を最小にしたときに縦に表示される
+                                    minRoation: 90, //横幅を最小にしたときに縦に表示される
+                                    fontSize:18
                                 },
                                 gridLines: {
                                     //x軸の網線
@@ -113,7 +126,4 @@
 </script>
 
 <style scoped>
-    .chart-small {
-        max-width: 600px;
-    }
 </style>
