3becbda2fb9e5e2a7e16bc6ea630f54ae6f7d82e
raige <raigekka0128@yahoo.co.jp>
2020-09-15 14:26:15 +0900

    +Update StartTop.vueの改変 -Delete StartTopImage.jpgの削除

--- src/components/StartTop.vue
+++ src/components/StartTop.vue
@@ -1,10 +1,26 @@
 <template>
     <div id="#app">
-        <div class="jumbotron-extend">
-            <div class="container-fluid jumbotron-container">
-                <h1>カロリーを管理してみませんか？</h1>
-                <p><a class="btn btn-outline-warning btn-lg" href="/#/signup" role="button">新規登録はこちら</a></p>
-            </div>
+        <div class="jumbotron jumbotron-fluid bg-dark">
+            <div class="container">
+                <h1 class="display-4 font-weight-lighter" style="font-family: Helvetica,'Yu Gothic UI',serif">カロリーを管理してみませんか？</h1>
+                <p class="lead">このアプリではカロリーを貯金出来ます！目に見てカロリーを管理したい方や最近体が気になってきた方におすすめです。
+                    詳しい機能は下の方に書いてあるのでぜひご覧ください！</p>
+                <hr class="my-4" style="background-color: white">
+                <div class="row">
+                    <p><a class="btn btn-success btn-lg mr-3" href="/#/login" role="button">ログインはこちら</a></p>
+                    <p><a class="btn btn-info btn-lg" href="/#/signup" role="button">新規登録はこちら</a></p>
+                </div>
+            </div><!-- /.container -->
+        </div><!-- /.jumbotron -->
+        <div class="container">
+            <h1>説明です！</h1>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .jumbotron {
+        background-position: center 60%;
+        color: white;
+    }
+</style>
