3f4245963f1e0a31cef8dcced2f8f582c248c44d
raige <raigekka0128@yahoo.co.jp>
2020-09-18 14:02:06 +0900

    -delete UserGuide.vueの削除 +add NotFound.vueの追加 +Update #を消す処理を追加

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import Router from 'vue-router'
 import StartTop from "../components/StartTop"
 import UserLogin from '../components/UserLogin'
 import UserSignUp from '../components/UserSignUp'
-import UserGuide from "../components/UserGuide"
 import SaveCalorie from '../components/SaveCalorie'
 import ConsumptionCalorieRegistration from "../components/ConsumptionCalorieRegistration"
 import IntakeCalorieRegistration from "../components/IntakeCalorieRegistration"
@@ -16,13 +15,17 @@ import UserChange from "../components/UserChange"
 
 // store
 import Store from '../store/index'
+import NotFound from "../components/NotFound";
 
 Vue.use(Router)
 
 const router = new Router({
-
+    //#を外すためhistory
+    mode: 'history',
     routes: [
         {
+            //Topページ
+            //ログイン前にある際とはmetaを追加する
             path: '/',
             name: 'StartTop',
             component: StartTop,
@@ -31,6 +34,7 @@ const router = new Router({
             }
         },
         {
+            //ログイン
             path: '/login',
             name: 'login',
             component: UserLogin,
@@ -39,6 +43,7 @@ const router = new Router({
             }
         },
         {
+            //新規登録
             path: '/signup',
             name: 'signup',
             component: UserSignUp,
@@ -47,48 +52,57 @@ const router = new Router({
             }
         },
         {
-            path: '/userguide',
-            name: 'userguide',
-            component: UserGuide,
-            meta: {
-                isPublic: true
-            }
-        },
-        {
+            //カロリー貯金
             path: '/savecalorie',
             component: SaveCalorie,
+            name:'savecalorie',
         },
         {
+            //消費カロリー入力
             path: '/consumptioncalorie',
             component: ConsumptionCalorieRegistration,
+            name: 'consumptioncalorie',
         },
         {
+            //摂取カロリー入力
             path: '/intakecalorie',
-            component: IntakeCalorieRegistration
+            component: IntakeCalorieRegistration,
+            name: 'intakecalorie',
         },
         {
+            //カレンダー
             path: "/calendar",
-            component: Calender
+            component: Calender,
+            name: 'calendar',
         },
         {
+            //トレーニング
             path: "/training",
             name: "training",
             component: Training,
         },
         {
+            //統計
             path: "/statistics",
             name: "statistics",
             component: Statistics,
         },
         {
+            //ユーザー情報変更
             path: "/userchange",
             name: "userchange",
             component: UserChange,
+        },
+        {
+            //NotFound
+            path: "/*",
+            name: 'notfound',
+            component: NotFound,
         }
     ]
 })
 
-
+// metaかトークンがないとログインに遷移する
 router.beforeEach((to, from, next) => {
     if (to.matched.some(page => page.meta.isPublic) || Store.state.accountToken) {
         next()
