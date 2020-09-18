118490877d22589e3677a704ad8698f37ddb0e35
raige <raigekka0128@yahoo.co.jp>
2020-09-18 15:59:45 +0900

    +update UpdateInformation.vue完成

--- src/components/UpdateInformation.vue
+++ src/components/UpdateInformation.vue
@@ -7,27 +7,47 @@
         </div><!-- /.jumbotron -->
         <div class="container">
             <div class="row">
-                <div class="col-2">
+                <div class="col-3 lead">
                     日付
                 </div>
-                <div class="col">
+                <div class="col lead ">
                     内容
                 </div>
             </div>
-            <div class="row">
-                <div class="col-2">
-                    8月20日
+            <hr>
+            <div class="row mb-3">
+                <div class="col-3 lead">
+                    9月17日
                 </div>
-                <div class="col">
-                    バグ修正、トレーニング・カレンダー・統計機能の追加
+                <div class="col lead">
+                    Twiiterアカウントを開設しました。新しく出来た機能や更新情報を呟いていきたいと思います。
+                    ぜひフォローしてください！
+                    <a href="https://twitter.com/acsys_calorie">Twitterアカウントはこちら</a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-2">
+            <div class="row mb-3">
+                <div class="col-3 lead">
+                    8月20日
+                </div>
+                <div class="col lead">
+                    <div class="mb-2">
+                        以下の機能を公開しました。<br>
+                        ・カレンダー<br>
+                        ・統計<br>
+                        ・トレーニング<br>
+                        ・登録情報の変更<br>
+                    </div>
+                    以下のバグを修正しました。<br>
+                    ・新しく摂取カロリーを登録すると、前に登録した摂取カロリーが消えること。<br>
+                    ・統計機能でリストにある貯金の表示ミスを修正しました。
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-3 lead">
                     8月14日
                 </div>
-                <div class="col">
-                    サイト公開
+                <div class="col lead">
+                    サイト公開しました。
                 </div>
             </div>
         </div>
