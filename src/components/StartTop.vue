46888af8c19a6001b0e46e90284f3790e1a2a44e
raige <raigekka0128@yahoo.co.jp>
2020-09-29 16:10:42 +0900

    +update StartTop.vue大枠完成 +update footerを追加

--- src/components/StartTop.vue
+++ src/components/StartTop.vue
@@ -1,17 +1,21 @@
 <template>
     <div id="#app">
         <div class="jumbotron jumbotron-fluid bg-dark">
-            <div class="container">
+            <div class="container text-center">
                 <h1 class="display-1 font-weight-lighter" style="font-family: Helvetica,'Yu Gothic UI',serif">acsys</h1>
                 <h3 class= "font-weight-lighter" style="font-family: Helvetica,'Yu Gothic UI',serif">-簡単にカロリー管理-</h3>
                 <hr class="my-4" style="background-color: white">
                 <div class="row">
-                    <p><a class="btn btn-success btn-lg mr-3" href="/login" role="button">ログイン</a></p>
-                    <p><a class="btn btn-info btn-lg" href="/signup" role="button">無料登録</a></p>
-                </div>
+                    <div class="col text-right">
+                        <p><a class="btn btn-success btn-lg" href="/login" role="button">ログイン</a></p>
+                    </div>
+                    <div class="col text-left">
+                        <p><a class="btn btn-info btn-lg" href="/signup" role="button">無料登録</a></p>
+                    </div>
+                </div><!-- /.row -->
             </div><!-- /.container -->
         </div><!-- /.jumbotron -->
-        <div class="container">
+        <div class="container marketing">
             <h2 class="text-center pt-4">あらゆるデバイスでカロリーを管理を簡単に</h2>
             <div class="row pt-5">
                 <div class="col-lg-4">
@@ -20,9 +24,9 @@
                             <path d="M5.75 13.5c.167-.333.25-.833.25-1.5h4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75z"/>
                             <path fill-rule="evenodd" d="M13.991 3H2c-.325 0-.502.078-.602.145a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3zM14 2H2C0 2 0 4 0 4v6c0 2 2 2 2 2h12c2 0 2-2 2-2V4c0-2-2-2-2-2z"/>
                         </svg>
-                    </div>
+                    </div><!-- /.text-center -->
                     <h5 class="pb-2 pt-1 font-weight-bold">場所やデバイスを問わずカロリー管理</h5>
-                    <p>Webアプリのため、検索してログインするだけでどこからでも管理が利用出来るようになります。</p>
+                    <p>Webアプリのため、どこからでもカロリー管理が利用出来るようになります。</p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
                     <div class="text-center mb-1">
@@ -31,7 +35,7 @@
                             <rect width="4" height="9" x="6" y="6" rx="1"/>
                             <rect width="4" height="14" x="11" y="1" rx="1"/>
                         </svg>
-                    </div>
+                    </div><!-- /.text-center -->
                     <h5 class="pb-2 pt-1 font-weight-bold">直感的にカロリー管理</h5>
                     <p>グラフでその日や月のカロリー情報を表示しているため、現在の状況をすぐ理解できます。</p>
                 </div><!-- /.col-lg-4 -->
@@ -40,18 +44,74 @@
                         <svg width="64" height="64" viewBox="0 0 16 16" class="bi bi-bicycle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5zm1.5 2.443l-.508.814c.5.444.85 1.054.967 1.743h1.139L5.5 6.943zM8 9.057L9.598 6.5H6.402L8 9.057zM4.937 9.5a1.997 1.997 0 0 0-.487-.877l-.548.877h1.035zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765l1.027-1.643zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53L11.55 8.623z"/>
                         </svg>
-                    </div>
+                    </div><!-- /.text-center -->
                     <h5 class="pb-2 pt-1 font-weight-bold">おすすめトレーニング</h5>
                     <p>カロリーをたくさん消費できるトレーニングを紹介しています。トレーニングを達成すると自動的に登録されます。</p>
                 </div><!-- /.col-lg-4 -->
             </div><!-- /.row -->
-        </div>
+
+            <hr class="featurette-divider  mt-5 pb-5">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">シンプルなデザイン<span class="text-muted"> 直感的に操作できる。</span></h2>
+                    <p class="lead">画像に表示されているのはカロリー貯金画面です。今までの貯金や摂取、消費カロリーがすぐにわかるようになっています。またこの画面からカロリーの入力画面には簡単に移ることが出来ます。</p>
+                </div>
+                <div class="col-md-5">
+                    <img src="../assets/topPage.png" class="img-fluid" alt="Responsive image">
+                </div>
+            </div><!-- /.row featurette -->
+            <hr class="featurette-divider  mt-5">
+        </div><!-- /.container -->
+        <footer class="container">
+            <div class="text-center">
+                <div class="sosyal">
+                    <a href="https://twitter.com/acsys_calorie" class="twitter">acsys公式Twitter</a>
+                </div>
+            </div>
+            <p class="float-right"><a href="/" class="btn btn-outline-success">トップに戻る</a></p>
+            <p>2020 麻生ビジネス専門学校情報工学科2班<br>
+                お問い合わせ:1701017@st.asojuku.ac.jp</p>
+        </footer><!-- /.footer -->
     </div>
 </template>
 
+<script>
+    export default {
+        name: "StartTop",
+    }
+</script>
+
 <style scoped>
     .jumbotron {
+        background-image: url("../assets/3275593_m.jpg");
+        background-size: cover;
         background-position: center 60%;
+        color: #ffffff;
+    }
+    .sosyal {
+        margin: 50px auto;
+    }
+    .twitter {
+        background-color: #008DDE;
+        box-shadow: 0 4px 0 #0078BD;
+
+        display: inline;
+        position: relative;
+        font-family: Arial;
+        font-size: 14px;
+        font-weight: bold;
+        text-align: center;
+        text-decoration: none;
         color: white;
+        border-radius: 5px;
+        padding: 14px 80px;
+    }
+    .twitter:active {
+        top: 2px;
+        box-shadow: 0 2px 0 #0078BD;
+    }
+    .twitter:hover {
+        background-color: #1397D8;
     }
 </style>
--- src/components/UpdateInformation.vue
+++ src/components/UpdateInformation.vue
@@ -13,7 +13,7 @@
                 <div class="col lead ">
                     内容
                 </div>
-            </div>
+            </div><!-- /.row -->
             <hr>
             <div class="row mb-3">
                 <div class="col-3 lead">
@@ -24,7 +24,7 @@
                     ぜひフォローしてください！
                     <a href="https://twitter.com/acsys_calorie">Twitterアカウントはこちら</a>
                 </div>
-            </div>
+            </div><!-- /.row mb-3 -->
             <div class="row mb-3">
                 <div class="col-3 lead">
                     8月20日
@@ -41,7 +41,7 @@
                     ・新しく摂取カロリーを登録すると、前に登録した摂取カロリーが消えること。<br>
                     ・統計機能でリストにある貯金の表示ミスを修正しました。
                 </div>
-            </div>
+            </div><!-- /.row mb-3 -->
             <div class="row mb-3">
                 <div class="col-3 lead">
                     8月14日
@@ -49,20 +49,50 @@
                 <div class="col lead">
                     サイト公開しました。
                 </div>
+            </div><!-- /.row mb-3 -->
+        </div><!-- /.container -->
+        <hr class="featurette-divider  mt-5">
+        <footer class="container">
+            <div class="text-center">
+                <div class="sosyal">
+                    <a href="https://twitter.com/acsys_calorie" class="twitter">acsys公式Twitter</a>
+                </div>
             </div>
-        </div>
+            <p class="float-right"><a href="/updateinformation" class="btn btn-outline-success">トップに戻る</a></p>
+            <p>2020 麻生ビジネス専門学校情報工学科2班<br>
+                お問い合わせ:1701017@st.asojuku.ac.jp</p>
+        </footer><!-- /.footer -->
     </div>
 </template>
-
-<script>
-    export default {
-        name: "UpdateInformation"
-    }
-</script>
 
 <style scoped>
     .jumbotron {
         background-position: center 60%;
         color: white;
+    }
+    .sosyal {
+        margin: 50px auto;
+    }
+    .twitter {
+        background-color: #008DDE;
+        box-shadow: 0 4px 0 #0078BD;
+
+        display: inline;
+        position: relative;
+        font-family: Arial;
+        font-size: 14px;
+        font-weight: bold;
+        text-align: center;
+        text-decoration: none;
+        color: white;
+        border-radius: 5px;
+        padding: 14px 80px;
+    }
+    .twitter:active {
+        top: 2px;
+        box-shadow: 0 2px 0 #0078BD;
+    }
+    .twitter:hover {
+        background-color: #1397D8;
     }
 </style>
