52244f292879a0c0014f38426bf32387e6a5c0fb
raige <raigekka0128@yahoo.co.jp>
2020-09-30 11:06:16 +0900

    +update npm install vue-analytics npm audit fix

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-178757623-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-178757623-1');
-</script>
-
 <template>
-  <div>
+    <div>
 <!--  headerの設定-->
       <LoginHeader v-if="this.$store.state.accountToken" ></LoginHeader>
       <TopHeader v-else-if="!this.$store.state.accountToken"></TopHeader>
       <transition mode="out-in">
           <router-view />
       </transition>
-  </div>
+    </div>
 </template>
 
 <script>
