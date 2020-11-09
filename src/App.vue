11580fa0cbbcbb304f71377e16b8ee553956b1c0
raige <raigekka0128@yahoo.co.jp>
2020-11-09 14:28:59 +0900

    +update アナリティクスの反応しないもんだい解決

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-178757623-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-178757623-1');
+</script>
+
 <template>
     <div>
 <!--  headerの設定-->
