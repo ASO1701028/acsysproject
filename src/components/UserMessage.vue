a77f8caa71a1c08ab88bf8d4624c0b057e67eff7
ASO1701028 <yuichi.adobe@gmail.com>
2020-11-18 09:53:43 +0900

    add アカウント再発行フォーム追加

---
+++ src/components/UserMessage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="#app" class="container">
+        <div class="text-success mt-5 pt-5 mb-4 text-center h1 font-weight-bold">
+            入力されたメールへ送信しました。ご確認ください。
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserMessage"
+    }
+</script>
+
+<style scoped>
+
+</style>
